300dfab01d87af6b7dd938237c3003bb1073edc3
Sang <sang.nguyen2000@hotmail.de>
2021-01-21 13:32:45 +0100

    implemented view binding

--- Vocabulary App/app/src/main/java/com/example/android/vocabularyapp/ui/category/CategoryActivity.kt
+++ Vocabulary App/app/src/main/java/com/example/android/vocabularyapp/ui/category/CategoryActivity.kt
@@ -4,19 +4,29 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.vocabularyapp.R
 import com.example.android.vocabularyapp.database.entities.LanguageDb
+import com.example.android.vocabularyapp.databinding.ActivityCategoryBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class CategoryActivity : AppCompatActivity() {
 
     private val viewModel by viewModel<CategoryViewModel>()
+    private lateinit var binding: ActivityCategoryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
+        binding = ActivityCategoryBinding.inflate(layoutInflater)
 
+        initOnClick()
 
-        val language = LanguageDb(0, "Deuscth", " -")
+        setContentView(binding.root)
 
-        viewModel.addLanguage(language)
+    }
+
+    private fun initOnClick() {
+        binding.floatingActionButton.setOnClickListener {
+            val language = LanguageDb(0, "Deutsch", " -")
+
+            viewModel.addLanguage(language)
+        }
     }
 }
